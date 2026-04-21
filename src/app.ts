589d142d15a03fe456465d7c87e0bdd22fad224b
Bernardo Damiani <bmdamiani17@gmail.com>
2026-04-21 09:13:33 -0300

    feat: add user info pipeline

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { errorHandler } from "./middlewares/errorHandler";
 import { authRouter } from "./modules/auth/auth.router";
 import { exercisesRouter } from "./modules/exercises/exercises.router";
 import { executionsRouter } from "./modules/executions/executions.router";
+import { usersRouter } from "./modules/users/users.router";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.get("/health", (_req, res) => {
 app.use("/auth", authRouter);
 app.use("/exercises", exercisesRouter);
 app.use("/executions", executionsRouter);
+app.use("/", usersRouter);
 
 app.use(errorHandler);
 
